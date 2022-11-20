659d2434c3ab6024026945af47a2f019e84a8171
Maximilian Kaske <maximilian@kaske.org>
2022-11-20 20:34:08 +0100

    feat: homepage tile

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,11 +1,14 @@
 import { getView } from "@/lib/redis/view-counter";
+import { allDocs } from "contentlayer/generated";
 import GettingStarted from "./getting-started";
+import Tile from "./tile";
 
 // TODO: add tiles with features on items
 
 export default async function Home() {
   // const counter = await getView("root");
   const NAME = process.env.NEXT_PUBLIC_DOCUMENTATION_NAME;
+  const docs = allDocs.filter((d) => d.url.startsWith("/data-types"));
   return (
     <div className="container mx-auto space-y-12 px-6 py-24">
       <div className="mx-auto max-w-2xl text-center">
@@ -17,6 +20,11 @@ export default async function Home() {
           <span className="text-[#7C3AED]">Contentlayer</span>.
         </h1>
         <GettingStarted />
+      </div>
+      <div className="mx-auto grid max-w-4xl grid-cols-1 gap-6 md:grid-cols-2">
+        {docs.map(({ url, title, excerpt }) => (
+          <Tile key={url} {...{ url, title, excerpt }} />
+        ))}
       </div>
     </div>
   );
---
+++ src/app/(marketing)/tile.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+interface Props {
+  url: string;
+  title: string;
+  excerpt: string;
+}
+
+export default function Tile({ url, title, excerpt }: Props) {
+  return (
+    <Link
+      href={url}
+      className="group overflow-hidden rounded-md border border-gray-100 hover:border-brand-300"
+    >
+      <h2 className="border-b border-gray-100 bg-gray-50 px-4 py-3 font-semibold group-hover:border-brand-100 group-hover:bg-brand-50">
+        {title}
+      </h2>
+      <p className="px-4 py-3">{excerpt}</p>
+    </Link>
+  );
+}
