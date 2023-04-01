7a032f9f3514a3c3b51e67cee0f6405cc0fd6e15
Maximilian Kaske <maximilian@kaske.org>
2023-04-01 12:19:51 +0200

    chore: include layout

--- apps/web/src/app/[...slug]/page.tsx
+++ apps/web/src/app/[...slug]/page.tsx
@@ -10,15 +10,5 @@ export async function generateStaticParams() {
 
 export default function StaticPage({ params }: { params: { slug: string[] } }) {
   const page = getPagefromSlug(params.slug);
-  return (
-    <div className="container mx-auto">
-      {/* REMOVED: border-gray-100 lg:border-l lg:border-r */}
-      <div className="mx-auto max-w-prose px-4 py-6 lg:px-6">
-        <Content content={page} />
-      </div>
-    </div>
-  );
+  return <Content content={page} />;
 }
-
-// {/* REMINDER: rsc_counter injects the Server Component into a Client Component! */}
-// {/* https://beta.nextjs.org/docs/rendering/server-and-client-components#importing-server-components-into-client-components */}
